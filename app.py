184dd21fca2e4a40e0cea54dd676bf6f3b3b0184
바림 <balim@balim-ui-iMac.local>
2022-01-23 23:37:57 +0900

    driver

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from distutils.log import debug
 from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Thank 섹스"
+def init():
+    return "Hello Thanks"
+
+driver = webdriver.Chrome()
+url = 'https://www.google.com'
+driver.get(url)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
